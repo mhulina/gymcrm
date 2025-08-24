954e6a1bb4df7cc2fcdbcaea987c42f961ad4307
Matija Hulina <matija.hulina@gmail.com>
2025-08-24 02:24:16 +0200

    Added HolidayRepository methods

--- GymCRM.SchedulingAPI/Infrastructure/Implementation/HolidayRepository.cs
+++ GymCRM.SchedulingAPI/Infrastructure/Implementation/HolidayRepository.cs
@@ -1,8 +1,72 @@
 ﻿using GymCRM.SchedulingAPI.Infrastructure.Interface;
+using GymCRM.SchedulingAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymCRM.SchedulingAPI.Infrastructure.Implementation;
 
 public class HolidayRepository : IHolidayRepository
 {
+    private readonly SchedulingDbContext _context;
+
+    public HolidayRepository(SchedulingDbContext context)
+    {
+        _context = context;
+    }
     
+    public async Task<List<Holiday>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var result = await _context.Holidays.ToListAsync(cancellationToken: cancellationToken);
+        
+        return result;
+    }
+
+    public async Task<Holiday> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _context.Holidays
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken: cancellationToken);
+        
+        return result;
+    }
+
+    public async Task<Holiday> GetByDateAsync(DateTime date, CancellationToken cancellationToken)
+    {
+        var result = await _context.Holidays
+            .FirstOrDefaultAsync(x => x.Date == date.Date, cancellationToken: cancellationToken);
+        
+        return result;
+    }
+
+    public async Task<List<Holiday>> GetByMonthAsync(DateTime date, CancellationToken cancellationToken)
+    {
+        var result = await _context.Holidays
+            .Where(x => x.Date.Month == date.Month 
+                && x.Date.Year == date.Year)
+            .ToListAsync(cancellationToken: cancellationToken);
+        
+        return result;
+    }
+
+    public async Task<List<Holiday>> GetByYearAsync(DateTime date, CancellationToken cancellationToken)
+    {
+        var result = await _context.Holidays
+            .Where(x => x.Year == date.Year)
+            .ToListAsync(cancellationToken: cancellationToken);
+        
+        return result;
+    }
+
+    public void Add(Holiday holiday)
+    {
+        _context.Holidays.Add(holiday);
+    }
+
+    public void Update(Holiday holiday)
+    {
+        _context.Holidays.Update(holiday);
+    }
+
+    public void Delete(Holiday holiday)
+    {
+        _context.Holidays.Remove(holiday);
+    }
 }
--- GymCRM.SchedulingAPI/Infrastructure/Interface/IHolidayRepository.cs
+++ GymCRM.SchedulingAPI/Infrastructure/Interface/IHolidayRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace GymCRM.SchedulingAPI.Infrastructure.Interface;
+﻿using GymCRM.SchedulingAPI.Models.Entities;
+
+namespace GymCRM.SchedulingAPI.Infrastructure.Interface;
 
 public interface IHolidayRepository
 {
-    
+    Task<List<Holiday>> GetAllAsync(CancellationToken cancellationToken);
+    Task<Holiday> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Holiday> GetByDateAsync(DateTime date, CancellationToken cancellationToken);
+    Task<List<Holiday>> GetByMonthAsync(DateTime date, CancellationToken cancellationToken);
+    Task<List<Holiday>> GetByYearAsync(DateTime date, CancellationToken cancellationToken);
+    void Add(Holiday holiday);
+    void Update(Holiday holiday);
+    void Delete(Holiday holiday);
 }
